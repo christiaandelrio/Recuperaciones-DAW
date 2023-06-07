5fd559bf4642b6c2aacbb8d1355801039e017dd2
christiaandelrio <60599988+christiaandelrio@users.noreply.github.com>
2023-06-07 12:47:08 +0200

    Add files via upload

---
+++ TAREAS/Servidor/dwes_tema_08/TAREA_08/claves.inc.php
@@ -0,0 +1,10 @@
+<?php
+// Incluyo mis propias claves
+$keyAEMET = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZWxyaW8ubGVtb3M0OUBnbWFpbC5jb20iLCJqdGkiOiI0ODMxZDBiNi04YjljLTQyNGItYmQ1Ny05Yzk3OTI0MGJjMGYiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTY4NDY2NDY5OSwidXNlcklkIjoiNDgzMWQwYjYtOGI5Yy00MjRiLWJkNTctOWM5NzkyNDBiYzBmIiwicm9sZSI6IiJ9.YdK6jNN9gos5cXANTHFttOfUIMsPd84L345ARj1zgBM";
+
+$keyOpenWeatherMap  = '9422ac2f6da4168cbc6c0e33cf230bdf';  
+
+$keyBing = "AhmOiljVCLRoDd75--yoaTb0Y6wHP3ruo2QOIhHXVqt4q750UNgsnH4GCWpY24yL";  
+
+$googleClientId     = '836114007956-vk7c5rjaus097r101a2rjg0v2smef5ci.apps.googleusercontent.com'; 
+$googleClientSecret = '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'; 
